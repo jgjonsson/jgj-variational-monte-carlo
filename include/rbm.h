3360399df1712a6b8446d30879a6e4f57f5a2542
jgjonsson <gab.jonsson@gmail.com>
2023-05-07 22:53:12 +0200

    New Wavefunction class for the simplest type Restricted Boltzmann machine. Gaussian-Binary without intercations.

---
+++ include/rbm.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+
+#include "wavefunction.h"
+#include "particle.h"
+
+/// @brief The SimpleRBM class is a gaussian product trial wave function.
+/// @details The trial wave function is a product of gaussian functions
+/// for each particle, and is independent of the other particles.
+class SimpleRBM : public WaveFunction
+{
+public:
+    /// @brief Constructor for the SimpleRBM class.
+    /// @param alpha Variational parameter present in the exponent.
+    SimpleRBM(double alpha);
+    /// @brief Evaluate the trial wave function.
+    /// @param particles Vector of particles.
+    /// @return The value of the trial wave function.
+    double evaluate(std::vector<std::unique_ptr<class Particle>> &particles);
+    /// @brief Compute the double derivative of the trial wave function over trial wave function.
+    /// @param particles Vector of particles.
+    /// @return The local value of Laplasian.
+    double computeLocalLaplasian(std::vector<std::unique_ptr<class Particle>> &particles);
+    /// @brief Efficiently compute ratio of evaluation of trial wave function.
+    /// @param particles_numerator Vector of particles in the numerator.
+    /// @param particles_denominator Vector of particles in the denominator.
+    double evaluateRatio(std::vector<std::unique_ptr<class Particle>> &particles_numerator,
+                         std::vector<std::unique_ptr<class Particle>> &particles_denominator);
+
+    std::vector<double> computeQuantumForce(std::vector<std::unique_ptr<class Particle>> &particles, size_t particle_index);
+    std::vector<double> computeLogPsiDerivativeOverParameters(std::vector<std::unique_ptr<class Particle>> &particles);
+};
---
+++ src/rbm.cpp
@@ -0,0 +1,100 @@
+#include <memory>
+#include <cmath>
+#include <cassert>
+
+#include "../include/rbm.h"
+#include "../include/particle.h"
+
+SimpleRBM::SimpleRBM(double alpha)
+{
+    assert(alpha >= 0);
+    m_numberOfParameters = 1;
+    m_parameters.push_back(alpha);
+}
+
+double SimpleRBM::evaluate(std::vector<std::unique_ptr<class Particle>> &particles)
+{
+    double psi = 1.0;
+    double alpha = m_parameters[0]; // alpha is the first and only parameter for now.
+
+    for (size_t i = 0; i < particles.size(); i++)
+    {
+        // Let's support as many dimensions as we want.
+        double r2 = 0;
+        for (size_t j = 0; j < particles[i]->getPosition().size(); j++)
+            r2 += particles[i]->getPosition()[j] * particles[i]->getPosition()[j];
+        // spherical ansatz
+        double g = exp(-alpha * r2);
+
+        // Trial wave function is product of g for all particles.
+        // f ignored for now, due to considering non interacting particles.
+        psi = psi * g;
+    }
+    return psi;
+}
+
+double SimpleRBM::computeLocalLaplasian(std::vector<std::unique_ptr<class Particle>> &particles)
+{
+    // The expression I got for a single laplasian is, in invariant form, follows:
+    // (4 * alpha^2 * r_i^2 - 2 * alpha * NDIM)
+    // so it takes to sum over all particles.
+    double alpha = m_parameters[0];
+    double sum_laplasian = 0.0;
+    for (size_t i = 0; i < particles.size(); i++)
+    {
+        double r2 = 0.0;
+        for (size_t j = 0; j < particles[i]->getPosition().size(); ++j)
+            r2 += particles[i]->getPosition()[j] * particles[i]->getPosition()[j];
+        sum_laplasian += 4 * alpha * alpha * r2 - 2 * alpha * particles[i]->getPosition().size();
+    }
+    return sum_laplasian;
+}
+
+double SimpleRBM::evaluateRatio(std::vector<std::unique_ptr<class Particle>> &particles_numerator, std::vector<std::unique_ptr<class Particle>> &particles_denominator)
+{
+    assert(particles_numerator.size() == particles_denominator.size());
+    double ratio = 1.0;
+    double alpha = m_parameters[0];
+
+    for (size_t i = 0; i < particles_numerator.size(); i++)
+    {
+        double r2_numerator = 0.0;
+        double r2_denominator = 0.0;
+        for (size_t j = 0; j < particles_numerator[i]->getPosition().size(); j++)
+        {
+            r2_numerator += particles_numerator[i]->getPosition()[j] * particles_numerator[i]->getPosition()[j];
+            r2_denominator += particles_denominator[i]->getPosition()[j] * particles_denominator[i]->getPosition()[j];
+        }
+        ratio *= exp(-alpha * (r2_numerator - r2_denominator));
+    }
+    return ratio;
+}
+
+std::vector<double> SimpleRBM::computeQuantumForce(std::vector<std::unique_ptr<class Particle>> &particles, size_t particle_index)
+{
+    double alpha = m_parameters[0];
+    std::vector<double> quantumForce = std::vector<double>();
+    std::vector<double> position = particles[particle_index]->getPosition();
+    for (int j = 0; j < position.size(); j++)
+    {
+        quantumForce.push_back(-4 * alpha * position[j]);
+    }
+
+    return quantumForce;
+}
+
+std::vector<double> SimpleRBM::computeLogPsiDerivativeOverParameters(std::vector<std::unique_ptr<class Particle>> &particles)
+{
+    double alpha = m_parameters[0];
+    std::vector<double> logPsiDerivativeOverParameters = std::vector<double>();
+    double sum = 0.0;
+    for (size_t i = 0; i < particles.size(); i++)
+    {
+        double r2 = 0.0;
+        for (size_t j = 0; j < particles[i]->getPosition().size(); j++)
+            r2 += particles[i]->getPosition()[j] * particles[i]->getPosition()[j];
+        sum += r2;
+    }
+    logPsiDerivativeOverParameters.push_back(-sum);
+    return logPsiDerivativeOverParameters;
+}
