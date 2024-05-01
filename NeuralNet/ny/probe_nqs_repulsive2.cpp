6e0a03ac3bc23fa0e5c62a262c092a24839d62a8
Gabriel Jonsson <gab.jonsson@gmail.com>
2024-05-01 18:41:19 +0200

    Capped adiabatic factor to 1. Removed wrongly squaring psi once too much.

--- NeuralNet/ny/probe_nqs_repulsive2.cpp
+++ NeuralNet/ny/probe_nqs_repulsive2.cpp
@@ -122,6 +122,7 @@ int main(int argc, char **argv)
 double alpha = 0.5;//m_parameters[0]; // alpha is the first and only parameter for now.
 double beta = 2.82843; // beta is the second parameter for now.
 double adiabaticFactor = (double)(count+1)/ (double)fixed_number_optimization_runs;
+adiabaticFactor = std::min(1.0, adiabaticFactor);
 cout << "Adiabatic factor: " << adiabaticFactor << endl;
             // Construct a unique pointer to a new System
             system = std::make_unique<System>(
