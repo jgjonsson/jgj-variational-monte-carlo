4221080964e1f52bddebafbed7c3856f53edc147
Gabriel Jonsson <gab.jonsson@gmail.com>
2024-05-18 18:47:53 +0200

    Refactored RBM code.

--- Project-2/Problem-2e/probe_nqs_repulsive_adam.cpp
+++ Project-2/Problem-2e/probe_nqs_repulsive_adam.cpp
@@ -21,6 +21,85 @@
 
 using namespace std;
 
+std::unique_ptr<Sampler> runParallellMonteCarloSimulation(unsigned int globalSeed, size_t numberOfMetropolisSteps, size_t MC_reduction, bool converged, size_t max_iterations, size_t count, int numThreads, double stepLength, size_t numberOfDimensions, size_t numberOfParticles, std::vector<double> params, double omega, double inter_strength, size_t rbs_M, size_t rbs_N, double hard_core_size, std::unique_ptr<Sampler> samplers[]) {
+
+    // Random number setup in the way recommended for parallell computing, at https://github.com/anderkve/FYS3150/blob/master/code_examples/random_number_generation/main_rng_in_class_omp.cpp
+    //  Use the system clock to get a base seed
+    unsigned int base_seed = chrono::system_clock::now().time_since_epoch().count();
+
+    #pragma omp parallel shared(samplers, count) // Start parallel region.
+    {
+        int thread_id = omp_get_thread_num();
+
+        // Seed the generator with a seed that is unique for this thread
+        unsigned int my_seed = base_seed + thread_id;
+        auto rng = std::make_unique<Random>(my_seed);
+
+        size_t numberOfMetropolisStepsPerGradientIteration = numberOfMetropolisSteps / MC_reduction * (converged | count == max_iterations - 1 ? MC_reduction : 1);
+        numberOfMetropolisStepsPerGradientIteration /= numThreads; // Split by number of threads.
+
+        std::unique_ptr<Sampler> sampler;
+        std::unique_ptr<System> system;
+
+        // Initialize particles
+        auto particles = setupRandomUniformInitialStateWithRepulsion(
+            stepLength,
+            hard_core_size,
+            numberOfDimensions,
+            numberOfParticles,
+            *rng);
+
+        // Construct a unique pointer to a new System
+        system = std::make_unique<System>(
+            // Construct unique_ptr to Hamiltonian
+            std::make_unique<CoulombHamiltonian>(omega, inter_strength),
+            // Construct unique_ptr to wave function
+            std::make_unique<SimpleRBM>(rbs_M, rbs_N, params, omega),
+            // Construct unique_ptr to solver, and move rng
+            std::make_unique<MetropolisHastings>(std::move(rng)),
+            // Move the vector of particles to system
+            std::move(particles));
+
+        // Run steps to equilibrate particles
+        auto acceptedEquilibrationSteps = system->runEquilibrationSteps(
+            stepLength,
+            numberOfMetropolisStepsPerGradientIteration);
+
+        // Run the Metropolis algorithm
+        samplers[thread_id] = system->runMetropolisSteps(
+            stepLength,
+            numberOfMetropolisStepsPerGradientIteration);
+    }
+
+    // Create a new Sampler object containing the average of all the others.
+    return std::unique_ptr<Sampler>(new Sampler(samplers, numThreads));
+}
+
+std::vector<double> optimizeAndUpdateParameters(std::vector<double>& params, std::unique_ptr<Sampler>& combinedSampler, AdamOptimizer& adamOptimizer, bool verbose, double& parameter_tolerance, size_t count) {
+    auto gradient = std::vector<double>(params.size());
+    for (size_t param_num = 0; param_num < params.size(); ++param_num)
+    {
+        gradient[param_num] = combinedSampler->getObservables()[2 + param_num];
+    }
+    auto NewParams = adamOptimizer.adamOptimization(params, gradient);
+    double sum = 0.0;
+    for (size_t i = 0; i < params.size(); ++i) {
+        double diff = NewParams[i] - params[i];
+        sum += diff * diff;
+    }
+    double meanSquareDifference = sum / params.size();
+
+    if (verbose)
+    {
+        cout << "Iteration " << count << endl;
+        cout << endl;
+    }
+
+    cout << "Tolerance " << parameter_tolerance << " Total change: " << meanSquareDifference << endl;
+
+    return NewParams;
+}
+
 int main(int argc, char **argv)
 {
     // This program is for experimenting with learning rate and number of hidden layers.
@@ -55,6 +134,7 @@ int main(int argc, char **argv)
 
     size_t numberOfEquilibrationSteps = numberOfMetropolisSteps / 10;
     double omega = 1.0; // Oscillator frequency.
+    double hard_core_size = 0.0043 / sqrt(omega); // Hard core size of particles, only used for initialisation of particle positions here, just to not make them start on top of each other.
 
     size_t MC_reduction = 100; // Number of MC steps to reduce by at intermediate steps
 
@@ -83,63 +163,9 @@ int main(int argc, char **argv)
 
     for (size_t count = 0; count < max_iterations; ++count)
     {
-        converged = count == fixed_number_optimization_runs; // TODO: hack for converge condition on set number of iterations
-
-        // Random number setup in the way recommended for parallell computing, at https://github.com/anderkve/FYS3150/blob/master/code_examples/random_number_generation/main_rng_in_class_omp.cpp
-        //  Use the system clock to get a base seed
-        unsigned int base_seed = chrono::system_clock::now().time_since_epoch().count();
-
-#pragma omp parallel shared(samplers, count) // Start parallel region.
-        {
-            int thread_id = omp_get_thread_num();
-
-            // Seed the generator with a seed that is unique for this thread
-            unsigned int my_seed = base_seed + thread_id;
-            auto rng = std::make_unique<Random>(my_seed);
-
-            size_t numberOfMetropolisStepsPerGradientIteration = numberOfMetropolisSteps / MC_reduction * (converged | count == max_iterations - 1 ? MC_reduction : 1);
-            numberOfMetropolisStepsPerGradientIteration /= numThreads; // Split by number of threads.
-
-            std::unique_ptr<Sampler> sampler;
-            std::unique_ptr<System> system;
-
-            // Initialize particles
-            auto particles = setupRandomUniformInitialState(
-                stepLength,
-                numberOfDimensions,
-                numberOfParticles,
-                *rng);
-
-            // Construct a unique pointer to a new System
-            system = std::make_unique<System>(
-                // Construct unique_ptr to Hamiltonian
-                std::make_unique<CoulombHamiltonian>(omega, inter_strength),
-                // Construct unique_ptr to wave function
-                std::make_unique<SimpleRBM>(rbs_M, rbs_N, params, omega),
-                // Construct unique_ptr to solver, and move rng
-                std::make_unique<MetropolisHastings>(std::move(rng)),
-                // Move the vector of particles to system
-                std::move(particles));
-
-            // Run steps to equilibrate particles
-            auto acceptedEquilibrationSteps = system->runEquilibrationSteps(
-                stepLength,
-                numberOfMetropolisStepsPerGradientIteration);
-
-            // Run the Metropolis algorithm
-            samplers[thread_id] = system->runMetropolisSteps(
-                stepLength,
-                numberOfMetropolisStepsPerGradientIteration);
-        }
-
-        // Create a new Sampler object containing the average of all the others.
-        combinedSampler = std::unique_ptr<Sampler>(new Sampler(samplers, numThreads));
-
-        // TODO: Code below contains a mess with commented out code, from tolerance test previously used.
-        // As it stands right now it will always run the set number of optimization, and
-        if (converged)
-            break;
-
+        combinedSampler = runParallellMonteCarloSimulation(parameter_seed, numberOfMetropolisSteps, MC_reduction, converged, max_iterations, count, numThreads, stepLength, numberOfDimensions, numberOfParticles, params, omega, inter_strength, rbs_M, rbs_N, hard_core_size, samplers);
+        params = optimizeAndUpdateParameters(params, combinedSampler, adamOptimizer, verbose, parameter_tolerance, count);
+        /*
         // Extract the gradient
         auto gradient = std::vector<double>(params.size());
         for (size_t param_num = 0; param_num < params.size(); ++param_num)
@@ -164,6 +190,7 @@ int main(int argc, char **argv)
         }
 
         cout << "Tolerance " << parameter_tolerance << " Total change: " << meanSquareDifference << endl;
+        */
     }
     // Output information from the simulation
     combinedSampler->printOutputToTerminal(verbose);
