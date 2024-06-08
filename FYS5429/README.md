f9586c346d08f1e2433f15eea49efd57a67b2f97
Gabriel Jonsson <gab.jonsson@gmail.com>
2024-06-08 16:23:33 +0200

    Adding the blocking script, and adding README and comments.

--- FYS5429/README.md
+++ FYS5429/README.md
@@ -7,9 +7,15 @@ Building the executable for Restricted Boltzmann Machine (RBM) with Neural Quant
 make -C .. app=FYS5429/rbm/probe_nqs_rbm_repulsive_adam
 ```
 
-Run RBS for 2 dimensions, 2 particles, 20 hidden nodes, 600 epochs, learning rate 0.01, and 50 million Monte Carlo cycles:
+Run RBS for 2 dimensions, 2 particles, 20 hidden nodes, 600 epochs, learning rate 0.01, and 54 million Monte Carlo cycles:
 ```
-time ../bin/FYS5429/rbm/probe_nqs_rbm_repulsive_adam.out 2 2 20 600 0.01 50000000
+time ../bin/FYS5429/rbm/probe_nqs_rbm_repulsive_adam.out 2 2 20 600 0.01 54000000
 ```
 To give an idea what run time to expect, this takes about 20 minutes on a 2.5 GHz Intel Core 13th gen i7, with 14 cores - 20 logical cores.
+
+This creates the file energies.csv
+To run resampling to calculate statistical error, run:
+```
+python3 blocking.py
+```
 
--- src/adam.cpp
+++ src/adam.cpp
@@ -2,6 +2,8 @@
 
 AdamOptimizer::AdamOptimizer(int paramSize, double learningRate) : m(paramSize, 0), v(paramSize, 0), beta1(0.9), beta2(0.999), epsilon(1e-8), learning_rate(learningRate), count(0) {}
 
+//Credits to Github Copilot for generating the code inside this method.
+
 std::vector<double> AdamOptimizer::adamOptimization(std::vector<double> params, std::vector<double>& gradient) {
     double total_change = 0.0;
     // Update the parameter using Adam optimizer
@@ -22,31 +24,9 @@ std::vector<double> AdamOptimizer::adamOptimization(std::vector<double> params, 
 }
 
 void AdamOptimizer::reset() {
+    //To be used for example when introducing external forces, so that optimizer does not keep old momentum and keep going in a bad diretion.
     std::fill(m.begin(), m.end(), 0);
     std::fill(v.begin(), v.end(), 0);
-    // If you're tracking the count, reset it to zero as well.
+    //Also reset the count in this case, which also affects speed the optimizer is going.
     count = 0;
 }
-
-/*
-AdamOptimizer::AdamOptimizer(int paramSize, double learningRate) : m(paramSize, 0), v(paramSize, 0), beta1(0.9), beta2(0.999), epsilon(1e-8), learning_rate(learningRate) {}
-
-    std::vector<double> adamOptimization(std::vector<double> params, std::vector<double>& gradient, int count) {
-        double total_change = 0.0;
-        // Update the parameter using Adam optimizer
-        for (size_t param_num = 0; param_num < params.size(); ++param_num)
-        {
-            // Update biased first and second moment estimates
-            m[param_num] = beta1 * m[param_num] + (1 - beta1) * gradient[param_num];
-            v[param_num] = beta2 * v[param_num] + (1 - beta2) * gradient[param_num] * gradient[param_num];
-            // Compute bias-corrected first and second moment estimates
-            double m_hat = m[param_num] / (1 - pow(beta1, count + 1));
-            double v_hat = v[param_num] / (1 - pow(beta2, count + 1));
-            // Update parameters
-            params[param_num] -= learning_rate * m_hat / (sqrt(v_hat) + epsilon);
-            total_change += fabs(learning_rate * m_hat / (sqrt(v_hat) + epsilon));
-        }
-        return params;
-    }
-;
-*/
